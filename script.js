003920a3c01c8165565ebf520f223a2b160fead3
haniyeKazemi <helmaeb26@gmail.com>
2026-07-16 22:26:25 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+{
+  /* <div class="card blue">
+<div
+  style="
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  "
+>
+  <div class="date">
+    <span class="day"> 13 </span>
+    <span class="weekday">Mon</span>
+  </div>
+  <img
+    src=""
+    alt=""
+    id="statusImageThreeDayAgo"
+    style="
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin: 0 20px;
+    "
+  />
+  <p id="statusImageThreeDayAgo">hhhhhhhhh</p>
+</div>
+<div>
+  <h3 style="color: rgba(259, 79, 79)" id="highDegreeThreeDayAgo">
+    12 c
+  </h3>
+  <h3 style="color: rgba(85, 85, 252)" id="lowDegreeThreeDayAgo">
+    7 c
+  </h3>
+</div>
+</div> */
+}
